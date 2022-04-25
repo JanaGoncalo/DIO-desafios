5b70f40b9ca710f68b27ade2728064813d6a841b
Janaína Gonçalo <103140022+JanaGoncalo@users.noreply.github.com>
2022-04-25 14:35:20 -0300

    commit

---
+++ Desafios Inicias Java - GFT Start Woman/Multiplicação Simples.java
@@ -0,0 +1,47 @@
+Desafio
+Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   
+
+Entrada
+A entrada contém 2 valores inteiros.
+
+Saída
+Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um espaço em branco antes e depois da igualdade.
+
+ 
+Exemplos de Entrada	Exemplos de Saída
+3                   PROD = 27
+9
+
+
+
+-30                 PROD = -300
+10
+
+
+
+0                   PROD = 0
+9
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class MultiplicacaoSimples {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    int b = Integer.parseInt(st.nextToken());
+    
+    int total = a * b;
+    System.out.println("PROD = " + total);
+  }
+}
+
+
+
+
